9916fc45785e33b90cb5f4adbdb8ca0eda25a5a4
eomiguel <155032115+eomiguel314@users.noreply.github.com>
2025-08-04 12:50:34 -0400

    ultrassonic sensor logic update

--- index.ino
+++ index.ino
@@ -1,81 +1,104 @@
-int limiar = 500;// ajustar por caussa da luminaçao
-
-const int sensores[] = {A0, A1 , A2};
+int limiar = 500;// lucas , ajuste dps com um codigo de leitura caso nao funcione
+const int sensores[] = {A0, A1, A2};
 const int num_sensores = 3;
 int read[num_sensores];
 
-int i = 0;
+const int trigPin = 7;
+const int echoPin = 8;
+long time;
+int distance;
 
-//definir pinos do motor
-
-class DCMotor {  
+class DCMotor {
   int spd = 255, pin1, pin2;
-  
-  public:  
-  
-    void Pinout(int in1, int in2){ // Pinout é o método para a declaração dos pinos que vão controlar o objeto motor
-      pin1 = in1;
-      pin2 = in2;
-      pinMode(pin1, OUTPUT);
-      pinMode(pin2, OUTPUT);
-      }   
-    void Speed(int in1){ // Speed é o método que irá ser responsável por salvar a velocidade de atuação do motor
-      spd = in1;
-      }     
-    void Forward(){ // Forward é o método para fazer o motor girar para frente
-      analogWrite(pin1, spd);
-      digitalWrite(pin2, LOW);
-      }   
-    void Backward(){ // Backward é o método para fazer o motor girar para trás
-      digitalWrite(pin1, LOW);
-      analogWrite(pin2, spd);
-      }
-    void Stop(){ // Stop é o metodo para fazer o motor ficar parado.
-      digitalWrite(pin1, LOW);
-      digitalWrite(pin2, LOW);
-      }
-   };
-   DCMotor Motor1, Motor2; 
+
+public:
+  void Pinout(int in1, int in2) {
+    pin1 = in1;
+    pin2 = in2;
+    pinMode(pin1, OUTPUT);
+    pinMode(pin2, OUTPUT);
+  }
+
+  void Speed(int in1) {
+    spd = in1;
+  }
+
+  void Forward() {
+    analogWrite(pin1, spd);
+    digitalWrite(pin2, LOW);
+  }
+
+  void Backward() {
+    digitalWrite(pin1, LOW);
+    analogWrite(pin2, spd);
+  }
+
+  void Stop() {
+    digitalWrite(pin1, LOW);
+    digitalWrite(pin2, LOW);
+  }
+};
+
+DCMotor Motor1, Motor2;
 
 void setup() {
-  for(i < num_sensores, i++){
+  Serial.begin(9600);
+  for (int i = 0; i < num_sensores; i++) {
     pinMode(sensores[i], INPUT);
-
-    Motor1.Pinout(5, 6);
-    Motor1.Pinout(9, 10);
   }
 
-  //pinout dos motor
-
-  Serial.begin(9600);
+  Motor1.Pinout(5, 6); 
+  Motor2.Pinout(9, 10);  
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
 
-void loop(){
-  for(i < num_sensores, i++){
+void loop() {a
+  for (int i = 0; i < num_sensores; i++) {
     read[i] = analogRead(sensores[i]);
   }
-  
-  bool esquerdo = read[0] < limiar;
+
+  bool esquerda = read[0] < limiar;
   bool centro = read[1] < limiar;
-  bool direito = read[2] < limiar;
+  bool direita = read[2] < limiar;
 
-  if(centro && !esquerdo && !direito ){
-    Motor1.Forward();
-    Motor2.Forward();
-  } else if(esquerda && !direta) {
-    Motor1.Forward();
-    Motor2.Stop();
-  }
-  } else if(!esquerda && direta) {
-    Motor1.Stop();
-    Motor2.Forward();
-  }
-  else{
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  time = pulseIn(echoPin, HIGH);
+  distance = time * 0.034 / 2;
+
+  if (distance < 10) {
     Motor1.Stop();
     Motor2.Stop();
+    delay(100);
+    Motor1.Backward();
+    Motor2.Backward();
+    delay(400);
+    Motor1.Stop();
+    Motor2.Forward();  // virar
+    delay(500);
+    Motor2.Stop();
+  } else {
+    if (centro && !esquerda && !direita) {
+      Motor1.Forward();
+      Motor2.Forward();
+    } else if (esquerda && !direita) {
+      Motor1.Forward();
+      Motor2.Stop();
+    } else if (!esquerda && direita) {
+      Motor1.Stop();
+      Motor2.Forward();
+    } else {
+      Motor1.Stop();
+      Motor2.Stop();
+    }
   }
-  Serial.print("E: "); Serial.print(leituras[0]);
-  Serial.print(" | C: "); Serial.print(leituras[1]);
-  Serial.print(" | D: "); Serial.println(leituras[2]);
 
-}
+  Serial.print("E: "); Serial.print(read[0]);
+  Serial.print(" | C: "); Serial.print(read[1]);
+  Serial.print(" | D: "); Serial.print(read[2]);
+  Serial.print(" | Dist: "); Serial.println(distance);
