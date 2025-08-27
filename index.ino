afb0190cb807bc1ff2f4db52ba8f4aada295df81
eomiguel <155032115+eomiguel314@users.noreply.github.com>
2025-08-27 10:51:12 -0500

    Update index.ino

--- index.ino
+++ index.ino
@@ -6,38 +6,50 @@
 #define sIr6 A5
 #define sIr7 A6
 
-int limiar = 46; // ------> Ajustar a limiar pra cada pista
+#define echo 10
+#define trig 11
+
+int limiar = 46;
+
+long tempo;
+float distancia;
 
 class DCMotor {
   int spd = 255, pin1, pin2;
 
-  public:
-    void Pinout(int in1, int in2) {
-      pin1 = in1;
-      pin2 = in2;
-      pinMode(pin1, OUTPUT);
-      pinMode(pin2, OUTPUT);
-    }
-    void Speed(int in1) {
-      spd = in1;
-    }
-    void Forward() {
-      analogWrite(pin1, spd);
-      digitalWrite(pin2, LOW);
-    }
-    void Backward() {
-      digitalWrite(pin1, LOW);
-      analogWrite(pin2, spd);
-    }
-    void Stop() {
-      digitalWrite(pin1, LOW);
-      digitalWrite(pin2, LOW);
-    }
+public:
+  void Pinout(int in1, int in2) {
+    pin1 = in1;
+    pin2 = in2;
+    pinMode(pin1, OUTPUT);
+    pinMode(pin2, OUTPUT);
+  }
+
+  void Speed(int in1) {
+    spd = in1;
+  }
+
+  void Forward() {
+    analogWrite(pin1, spd);
+    digitalWrite(pin2, LOW);
+  }
+
+  void Backward() {
+    digitalWrite(pin1, LOW);
+    analogWrite(pin2, spd);
+  }
+
+  void Stop() {
+    digitalWrite(pin1, LOW);
+    digitalWrite(pin2, LOW);
+  }
 };
 
 DCMotor Motor1, Motor2;
 
 void setup() {
+  Serial.begin(9600);
+
   Motor1.Pinout(2, 3);
   Motor2.Pinout(4, 5);
 
@@ -49,10 +61,32 @@ void setup() {
   pinMode(sIr6, INPUT);
   pinMode(sIr7, INPUT);
 
-  Serial.begin(9600);  
+  pinMode(echo, INPUT);
+  pinMode(trig, OUTPUT);
 }
 
 void loop() {
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+
+  tempo = pulseIn(echo, HIGH);
+  distancia = tempo * 0.034 / 2;
+    Serial.println(distancia);
+  if (distancia < 15) {
+    Motor1.Forward();
+    Motor2.Stop();
+
+  } else {
+    SeguirLinha();
+  }
+
+  delay(100);
+}
+
+void SeguirLinha() {
   int IR1 = analogRead(sIr1);
   int IR2 = analogRead(sIr2);
   int IR3 = analogRead(sIr3);
@@ -61,67 +95,71 @@ void loop() {
   int IR6 = analogRead(sIr6);
   int IR7 = analogRead(sIr7);
 
-  Serial.print(IR1); Serial.print(" ");
-  Serial.print(IR2); Serial.print(" ");
-  Serial.print(IR3); Serial.print(" ");
-  Serial.print(IR4); Serial.print(" ");
-  Serial.print(IR5); Serial.print(" ");
-  Serial.print(IR6); Serial.print(" ");
-  Serial.println(IR7);
+  int L1 = IR1 > limiar ? 1 : 0;
+  int L2 = IR2 > limiar ? 1 : 0;
+  int L3 = IR3 > limiar ? 1 : 0;
+  int L4 = IR4 > limiar ? 1 : 0;
+  int L5 = IR5 > limiar ? 1 : 0;
+  int L6 = IR6 > limiar ? 1 : 0;
+  int L7 = IR7 > limiar ? 1 : 0;
 
-  if (IR1 < limiar && IR2 < limiar && IR3 > limiar && IR4 > limiar && IR5 > limiar && IR6 < limiar && IR7 < limiar) {
-    // Frente -> 0 0 1 1 1 0 0
+  Serial.print(L1); Serial.print(" ");
+  Serial.print(L2); Serial.print(" ");
+  Serial.print(L3); Serial.print(" ");
+  Serial.print(L4); Serial.print(" ");
+  Serial.print(L5); Serial.print(" ");
+  Serial.print(L6); Serial.print(" ");
+  Serial.println(L7);
+
+
+  if (L3 == 1 && L4 == 1 && L5 == 1 && L1 == 0 && L2 == 0 && L6 == 0 && L7 == 0) {
     Motor1.Forward();
     Motor2.Forward();
-    Serial.print("Frente");
-  } 
-  else if (IR1 < limiar && IR2 < limiar && IR3 < limiar && IR4 > limiar && IR5 > limiar && IR6 < limiar && IR7 < limiar) {
-    // Frente -> 0 0 0 1 1 0 0 
-    Motor1.Forward();
+    Serial.println("Seguindo em frente");
+  }
+  else if (L5 == 1 && L6 == 1 && L7 == 1) {
+    Motor1.Backward();
     Motor2.Forward();
-    Serial.print("Frente");
-  } 
-  else if (IR1 < limiar && IR2 < limiar && IR3 > limiar && IR4 > limiar && IR5 < limiar && IR6 < limiar && IR7 < limiar) {
-    // Frente -> 0 0 1 1 0 0 0
+    Serial.println("Curva esquerda 1");
+  }
+  else if (L1 == 1 && L2 == 1 && L3 == 1) {
     Motor1.Forward();
-    Motor2.Forward();
-    Serial.print("Frente");
-  } 
-  else if (IR1 < limiar && IR2 < limiar && IR3 < limiar && IR4 > limiar && IR5 > limiar && IR6 > limiar && IR7 < limiar) {
-    // Pouco esquerda -> 0 0 0 1 1 1 0
+    Motor2.Backward();
+    Serial.println("Curva direita 1");
+  }
+  else if (L5 == 1 && L6 == 1 && L1 == 0 && L2 == 0) {
     Motor1.Stop();
     Motor2.Forward();
-  } 
-  else if (IR1 < limiar && IR2 < limiar && IR3 < limiar && IR4 < limiar && IR5 > limiar && IR6 > limiar && IR7 < limiar) {
-    // Mais esquerda -> 0 0 0 0 1 1 0 
-    Motor1.Backward();
-    Motor2.Forward();
+    Serial.println("Curva esquerda 0.5");
   }
-  else if (IR1 < limiar && IR2 < limiar && IR3 < limiar && IR4 < limiar && IR5 > limiar && IR6 > limiar && IR7 > limiar) {
-    // Forte esquerda -> 0 0 0 0 1 1 1 
-    Motor1.Backward();
-    Motor2.Forward();
-  }  
-  else if (IR1 < limiar && IR2 < limiar && IR3 < limiar && IR4 < limiar && IR5 < limiar && IR6 < limiar && IR7 > limiar) {
-    // Linha no canto direito -> 0 0 0 0 0 0 1 
-    Motor1.Backward();
-    Motor2.Forward();
+  else if (L1 == 1 && L2 == 1 && L6 == 0 && L7 == 0) {
+    Motor1.Forward();
+    Motor2.Stop();
+    Serial.println("cruva direita 0.5");
   }
-  else if(IR1 < limiar && IR2 < limiar && IR3 < limiar && IR4 < limiar && IR5 < limiar && IR6 < limiar && IR7 < limiar){
-    // Linha perdida
+  else if (L1 == 1 && L2 == 1 && L3 == 1 && L4 == 1 && L5 == 1 && L6 == 1 && L7 == 1) {
+    Motor1.Stop();
+    Motor2.Stop();
+    Serial.println("cruzamernto detectado");
+    delay(300);
+  }
+  else if (L2 == 1 && L3 == 1 && L4 == 1 && L5 == 1 && L6 == 1) {
+    // Cruzamento em T
+    Motor1.Stop();
+    Motor2.Stop();
+    Serial.println(" T");
+    delay(300);
+  }
+  else if (L1 == 0 && L2 == 0 && L3 == 0 && L4 == 0 && L5 == 0 && L6 == 0 && L7 == 0) {
+    // Gap ou fora da pista
     Motor1.Backward();
     Motor2.Backward();
+    Serial.println("gap ou fora da linha");
+    delay(200);
   }
-  else if(IR1 > limiar && IR2 > limiar && IR3 > limiar && IR4 > limiar && IR5 > limiar && IR6 > limiar && IR7 > limiar){
-    // Linha perdida
-    Motor1.Backward();
-    Motor2.Backward();
+  else {
+    Motor1.Stop();
+    Motor2.Stop();
+    Serial.println("vermelho");
   }
-   else {
-  Motor2.Stop();
-  Motor1.Stop();
-  }
-
-  delay(100);
 }
- 
